535f99196a1990e4c4a5fbbfc179830032ad87be
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-18 02:43:10 -0300

    Refactor App component to enhance layout responsiveness
    - Updated the main element in App.jsx to include additional Tailwind CSS classes for improved responsiveness on different screen sizes.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "./components/Navbar";
 
 function App() {
 	return (
-		<main className="bg-black">
+		<main className="bg-black min-h-lvh md:min-h-dvh">
 			<Navbar />
 			<Hero />
 			<Highlights />
