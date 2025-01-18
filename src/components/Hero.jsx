f8b769062c4f5f12b7db03947ac195123850f515
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-18 02:32:00 -0300

    Update favicon, enhance Navbar and Hero components, and add GSAP for animations
    - Replaced favicon link in index.html with favicon.ico.
    - Added GSAP and @gsap/react dependencies in package.json for animation support.
    - Implemented responsive video handling in Hero component based on window size.
    - Enhanced Navbar component with dynamic navigation items and improved layout.
    - Fixed CSS class name from .scrim-max-width to .screen-max-width in index.css.
    - Updated bun.lockb and added new video assets for the project.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,48 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useState } from "react";
+import { heroVideo, smallHeroVideo } from "@/utils";
+import { useEffect } from "react";
+
 export const Hero = () => {
-	return <div>Hero</div>;
+	const [videoSrc, setVideoSrc] = useState(window.innerWidth < 768 ? smallHeroVideo : heroVideo);
+
+	const handleVideoSrcSet = () => {
+		if (window.innerWidth < 768) {
+			setVideoSrc(smallHeroVideo);
+		} else {
+			setVideoSrc(heroVideo);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", handleVideoSrcSet);
+		return () => {
+			window.removeEventListener("resize", handleVideoSrcSet);
+		};
+	}, []);
+
+	useGSAP(() => {
+		gsap.to("#hero", {
+			opacity: 1,
+			duration: 1,
+			ease: "power1.inOut"
+		});
+	}, []);
+
+	return (
+		<section className="w-full nav-height bg-black relative">
+			<div className="h-5/6 w-gull flex-center flex-col">
+				<p id="hero" className="hero-title">
+					iPhone 15 Pro
+				</p>
+				<div className="md:w-10/12 w-9/12">
+					<video autoPlay muted className="w-full">
+						<source src={videoSrc} type="video/webm" />
+					</video>
+				</div>
+			</div>
+			<div id="cta" className="flex flex-col items-center opacity-0 translate-y-20" />
+		</section>
+	);
 };
