30f508afa09e033693f4cccf79af2fb685738ea3
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-18 22:27:35 -0300

    Enhance Hero and Highlights components with GSAP animations and improve video handling
    - Added a function in Hero component to set video source based on window size.
    - Updated GSAP animations for smoother transitions in Hero and Highlights components.
    - Introduced new structure in Highlights component with animated title and links.
    - Added VideoCarousel component as a new file for future video carousel implementation.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,11 @@ import { useEffect } from "react";
 export const Hero = () => {
 	const [videoSrc, setVideoSrc] = useState(window.innerWidth < 768 ? smallHeroVideo : heroVideo);
 
+	/**
+	 * Sets the video source based on the window's inner width.
+	 * If the window's inner width is less than 768, sets the video source to smallHeroVideo.
+	 * If the window's inner width is greater than or equal to 768, sets the video source to heroVideo.
+	 */
 	const handleVideoSrcSet = () => {
 		if (window.innerWidth < 768) {
 			setVideoSrc(smallHeroVideo);
@@ -23,11 +28,8 @@ export const Hero = () => {
 	}, []);
 
 	useGSAP(() => {
-		gsap.to("#hero", {
-			opacity: 1,
-			duration: 1,
-			ease: "power1.inOut"
-		});
+		gsap.to("#hero", { opacity: 1, delay: 2, ease: "power1.inOut" });
+		gsap.to("#cta", { opacity: 1, y: -50, delay: 2, ease: "power1.inOut" });
 	}, []);
 
 	return (
@@ -37,12 +39,17 @@ export const Hero = () => {
 					iPhone 15 Pro
 				</p>
 				<div className="md:w-10/12 w-9/12">
-					<video autoPlay muted key={videoSrc} className="w-full h-full">
+					<video autoPlay muted key={videoSrc} playsInline className="pointer-events-none">
 						<source src={videoSrc} type="video/webm" />
 					</video>
 				</div>
 			</div>
-			<div id="cta" className="flex flex-col items-center opacity-0 translate-y-20" />
+			<div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+				<a href="#highlights" className="btn">
+					Buy
+				</a>
+				<p className="font-normal text-xl">From $199/month or $999</p>
+			</div>
 		</section>
 	);
 };
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,3 +1,35 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import { watchImg, rightImg } from "@/utils";
+
 export const Highlights = () => {
-	return <div>Highlights</div>;
+	useGSAP(() => {
+		gsap.to("#title", { opacity: 1, y: 0, duration: 1, ease: "power1.inOut" });
+		gsap.to(".link", { opacity: 1, y: 0, duration: 1, ease: "power1.inOut", stagger: 0.25 });
+	}, []);
+
+	return (
+		<section id="highlights" className="w-dvw overflow-hidden h-full common-padding bg-zinc">
+			<div className="screen-max-width">
+				<div className="mb-12 w-full md:flex items-end justify-between">
+					<h1 id="title" className="section-heading">
+						Get the highlights.
+					</h1>
+					<div className="flex flex-wrap items-end gap-5">
+						<p className="link">
+							Watch the film
+							<img src={watchImg} alt="Watch Icon" width={15} height={15} className="ml-2" />
+						</p>
+						<p className="link">
+							Watch the event
+							<img src={rightImg} alt="Right Icon" width={10} height={10} className="ml-2" />
+						</p>
+					</div>
+				</div>
+
+				{/* Carousel videos */}
+			</div>
+		</section>
+	);
 };
